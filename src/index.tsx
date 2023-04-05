fbb609ccca685c2c29ab51374a6dc7c202e7d117
Hắc Tún <khactuans.tran@gmail.com>
2023-04-05 12:10:12 +0700

    init redux toolkit, query base

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import { createRoot } from 'react-dom/client';
 // Axios
 import axios from 'axios';
 import { Chart, registerables } from 'chart.js';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+
+// Redux
+import store, { persistor } from 'app/reducers';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 // Apps
 import { MetronicI18nProvider } from './_metronic/i18n/Metronici18n';
 /**
@@ -31,17 +35,17 @@ import { AuthProvider, setupAxios } from './app/modules/auth';
 setupAxios(axios);
 Chart.register(...registerables);
 
-const queryClient = new QueryClient();
 const container = document.getElementById('root');
 if (container) {
   createRoot(container).render(
-    <QueryClientProvider client={queryClient}>
-      <MetronicI18nProvider>
-        <AuthProvider>
-          <AppRoutes />
-        </AuthProvider>
-      </MetronicI18nProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MetronicI18nProvider>
+          <AuthProvider>
+            <AppRoutes />
+          </AuthProvider>
+        </MetronicI18nProvider>
+      </PersistGate>
+    </ReduxProvider>
   );
 }
