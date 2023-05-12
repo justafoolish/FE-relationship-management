fb6358aad2278f54aadf3d6f172d6c993447fb62
Hắc Tún <khactuans.tran@gmail.com>
2023-05-12 19:03:13 +0700

    temo

--- src/app/components/form/AddPeopleForm.tsx
+++ src/app/components/form/AddPeopleForm.tsx
@@ -1,16 +1,53 @@
+import { PlusOutlined } from '@ant-design/icons';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Button as AntdButton, Divider, Input, Select, Space } from 'antd';
 import AvatarButton from 'app/components/button/AvatarButton';
 import FormControl from 'app/components/form-control/FormControl';
 import { FORM_CONTROLS } from 'app/domains/components/form.i';
-import { FC } from 'react';
-import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
-import * as Yup from 'yup';
-import Button from '../button';
 import { handleQueryError } from 'app/modules/utils/error-handler';
 import { useCreateRelationshipMutation } from 'app/reducers/relationship/relationship.api';
 import dayjs from 'dayjs';
+import { FC, useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
+import * as Yup from 'yup';
+import Button from '../button';
 import { IDialogBody } from '../dialog/CustomDialog';
+
+const CustomTagInput: FC = () => {
+  const [items, setItems] = useState(['jack', 'lucy']);
+  const [name, setName] = useState('');
+
+  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const addItem = (e: any) => {
+    // e.preventDefault();
+    setItems([...items, name]);
+    setName('');
+  };
+
+  return (
+    <Select
+      style={{ width: 300 }}
+      placeholder="custom dropdown render"
+      dropdownRender={(menu) => (
+        <>
+          {menu}
+          <Divider style={{ margin: '8px 0' }} />
+          <Space style={{ padding: '0 8px 4px' }}>
+            <Input placeholder="Please enter item" value={name} onChange={onNameChange} />
+            <AntdButton type="text" icon={<PlusOutlined />} onClick={addItem}>
+              Add item
+            </AntdButton>
+          </Space>
+        </>
+      )}
+      options={items.map((item) => ({ label: item, value: item }))}
+    />
+  );
+};
 
 interface IAddPeopleFormFields {
   name: string;
@@ -62,7 +99,9 @@ const AddPeopleForm: FC<IDialogBody> = ({ closeModal, callback }) => {
       <form className="h-100 d-flex flex-column" onSubmit={methods.handleSubmit(_submitForm)}>
         <div>
           <AvatarButton name="avatar" />
+          <CustomTagInput />
           {[
+            { name: 'name', placeholder: 'Enter name', label: 'Name', type: FORM_CONTROLS.TEXT },
             { name: 'name', placeholder: 'Enter name', label: 'Name', type: FORM_CONTROLS.TEXT },
             { name: 'date', placeholder: '', label: 'Date', type: FORM_CONTROLS.DATE_PICKER },
             { name: 'email', placeholder: 'Enter email', label: 'Email', type: FORM_CONTROLS.MAIL },
@@ -72,10 +111,7 @@ const AddPeopleForm: FC<IDialogBody> = ({ closeModal, callback }) => {
             return (
               <FormControl
                 key={idx}
-                type={formItem.type}
-                name={formItem.name}
-                label={formItem.label}
-                placeholder={formItem.placeholder}
+                {...formItem}
                 cxContainer="fv-row mb-8"
                 className="form-control form-control-lg form-control-solid "
               />
